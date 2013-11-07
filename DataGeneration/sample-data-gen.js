aa879b4c5b235ce30283733ed4c071ffef85fd12
David-Longshore <david.longshore@telerik.com>
2013-11-07 11:44:45 -0500

    data gen from hackathon groups

--- DataGeneration/sample-data-gen.js
+++ DataGeneration/sample-data-gen.js
@@ -1,40 +1,28 @@
 var random = require('./randomUtils');
+var people = require('./hackathonGroups');
 
-var nonDegenerateLink = function (list, categories) {
+var nonDegenerateLink = function (list) {
 	var sourceIndex = random.intUpTo(list.length);
 
 	return {
-		from: sourceIndex,
-		to: random.intUpToNexceptFor(list.length, sourceIndex),
-		topic: random.from(categories)
+		source: sourceIndex,
+		target: random.intUpToNexceptFor(list.length, sourceIndex),
+		value: random.intUpTo(3) + random.intUpTo(4)
 	};
 };
 
 var avgLinksPerPerson = 3;
 
-var people = [
-	{name: "Alice"},
-	{name: "Bob"},
-	{name: "Clair"},
-	{name: "Daniel"},
-	{name: "Elise"},
-	{name: "Fredrick"},
-	{name: "Gytha"},
-	{name: "Havelock"},
-	{name: "Ilya"},
-	{name: "Johnathon"}
-];
-
-var topics = [
-	".NET",
-	"JavaScript",
-	"HR question"
-];
+// var topics = [
+// 	".NET",
+// 	"JavaScript",
+// 	"HR question"
+// ];
 
 var links = [];
 
 for (var i = people.length * avgLinksPerPerson; i > 0; i--) {
-	links.push(nonDegenerateLink(people, topics));
+	links.push(nonDegenerateLink(people));
 }
 
 console.log(JSON.stringify({
