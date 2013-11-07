f7973832af46ca11592d19971ed16dd3c5efe14e
David-Longshore <david.longshore@telerik.com>
2013-11-07 12:25:42 -0500

    teamified data

--- DataGeneration/sample-data-gen.js
+++ DataGeneration/sample-data-gen.js
@@ -7,11 +7,11 @@ var nonDegenerateLink = function (list) {
 	return {
 		source: sourceIndex,
 		target: random.intUpToNexceptFor(list.length, sourceIndex),
-		weight: random.intUpTo(3) + random.intUpTo(4)
+		weight: (random.intUpTo(3) + random.intUpTo(4))
 	};
 };
 
-var avgLinksPerPerson = 3;
+var avgLinksPerPerson = 1;
 
 // var topics = [
 // 	".NET",
@@ -25,12 +25,29 @@ for (var i = people.length * avgLinksPerPerson; i > 0; i--) {
 	links.push(nonDegenerateLink(people));
 }
 
+var modelPeople = people.map(function (person) {
+	return {
+		name: person.Name,
+		team: person.TeamNumber
+	};
+});
+
+var numPeople = modelPeople.length;
+
+for (var fromIndex = 0; fromIndex < numPeople; fromIndex++) {
+	for (var toIndex = fromIndex + 1; toIndex < numPeople; toIndex++) {
+		if (modelPeople[fromIndex].team === modelPeople[toIndex].team)
+		{
+			links.push({
+				source: fromIndex,
+				target: toIndex,
+				weight: 3
+			})
+		}
+	}
+}
+
 console.log(JSON.stringify({
-	nodes: people.map(function (person) {
-		return {
-			name: person.Name,
-			team: person.TeamNumber
-		};
-	}),
+	nodes: modelPeople,
 	links: links
 }));
