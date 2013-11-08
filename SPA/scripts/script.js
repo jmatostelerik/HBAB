bacb4a797a85f493e243f9c968d0514e1152c121
jmatostelerik <john.matos@telerik.com>
2013-11-07 22:50:45 -0600

    color key works moar goodar

--- SPA/scripts/script.js
+++ SPA/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$(".roleSelect").on("change", updateForceGraphOpts);
+	$(".colorizeSelect").on("change", updateForceGraphOpts);
 
 });
 
@@ -8,13 +8,20 @@ $(document).ready(function(){
 
 
 // define some enumerators to map colors to keys
-var roles = [
-	"Engineering",
-	"Design",
-	"Business"
-	],
-	locations = [
-		"Europe",
-		"APAC",
-		"North America"
-	];
+var enumerations = {
+	role: "Engineering,Design,Business".split(","),
+	location: "Europe,APAC,North America".split(","),
+	team: "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17".split(",")
+};
+
+
+
+
+function updateColorKey(arr){
+	var html = ["<div>Color Key</div>"];
+	arr.forEach(function(str, i){
+		html.push("<div class='colorKeyBlock' style='background-color: "+ color(i) +";'></div>");
+		html.push("<div class='colorKeyValue'>"+ str +"</div><br>");
+	});
+	$("#colorKey").html(html.join(""));
+}
