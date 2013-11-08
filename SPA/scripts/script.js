9a51f0b7a32db88e0ba96547ee2eb07632769fd2
jmatostelerik <john.matos@telerik.com>
2013-11-07 22:25:06 -0600

    colors!

--- SPA/scripts/script.js
+++ SPA/scripts/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+	$(".roleSelect").on("change", updateForceGraphOpts);
 
 });
+
+
+
+
+// define some enumerators to map colors to keys
+var roles = "Engineering,Design,Business".split(","),
+	locations = "Europe,APAC,North America".split(",");
