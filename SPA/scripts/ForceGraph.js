90760eedd3c6b64969c4103145a89b7ab4fa630b
David-Longshore <david.longshore@telerik.com>
2013-11-07 23:25:52 -0500

    minor refactoring
    also indentation change :(

--- SPA/scripts/ForceGraph.js
+++ SPA/scripts/ForceGraph.js
@@ -1,87 +1,91 @@
-var newForceGraph = function(pristineData, selector, height, width) {
-	var UIDkey = function (item) { return item.UID; };
+(function () {
+	var funcOrZeroIfNot = function(func, predicate) {
+		return function (item) {
+			return predicate(item) ? func(item) : 0;
+		}
+	};
 
-	var vertices = [];
-	vertices.byUID = {};
-	var edges = [];
+	window.newForceGraph = function(pristineData, selector, height, width) {
+		var UIDkey = function (item) { return item.UID; };
 
-	pristineData.people.forEach(function (person) {
-		var newVertex = {
-			UID: person.UID,
-			person: person		
-		};
+		var vertices = [];
+		vertices.byUID = {};
+		var edges = [];
 
-		vertices.push(newVertex);
-		vertices.byUID[newVertex.UID] = newVertex;
-	});
-
-	pristineData.relationships.forEach(function (relationship) {
-		edges.push({
-			UID: relationship.UID,
-			relationship: relationship,
-			source: vertices.byUID[relationship.sourceUID],
-			target: vertices.byUID[relationship.targetUID]
-		});
-	});
-
-	var svg = d3.select(selector).append("svg").attr("width", width).attr("height", height);
-	var domVertices = function () { return svg.selectAll(".node"); };
-	var domEdges = function () { return svg.selectAll(".link"); };
-
-	var forceWeb = d3.layout.force()
-		.nodes(vertices)
-		.links(edges)
-		.size([width, height])
-		.on("tick", function (){
-			domVertices().attr("cx", function (vertex) { return vertex.x; })
-			   		     .attr("cy", function (vertex) { return vertex.y; });
-		   	
-		   	domEdges().attr("x1", function (edge) { return edge.source.x; })
-		   	          .attr("y1", function (edge) { return edge.source.y; })
-		   	          .attr("x2", function (edge) { return edge.target.x; })
-		   	          .attr("y2", function (edge) { return edge.target.y; });
-		});
-
-	return {
-		update: function (options) {
-			var edgeIsIncluded = function (edge) {
-				return options.nodeFilter(edge.source) && options.nodeFilter(edge.target);
+		pristineData.people.forEach(function (person) {
+			var newVertex = {
+				UID: person.UID,
+				person: person		
 			};
 
-			edgeOn = domEdges().data(edges.filter(edgeIsIncluded), UIDkey);
-			
-			edgeOn.enter()
-				.append("line")
-				.attr("class", "link");
+			vertices.push(newVertex);
+			vertices.byUID[newVertex.UID] = newVertex;
+		});
 
-			edgeOn.exit().remove();
+		pristineData.relationships.forEach(function (relationship) {
+			edges.push({
+				UID: relationship.UID,
+				relationship: relationship,
+				source: vertices.byUID[relationship.sourceUID],
+				target: vertices.byUID[relationship.targetUID]
+			});
+		});
 
-			edgeOn.style("stroke-width", options.strokeWidth);
+		var svg = d3.select(selector).append("svg").attr("width", width).attr("height", height);
+		var domVertices = function () { return svg.selectAll(".node"); };
+		var domEdges = function () { return svg.selectAll(".link"); };
+
+		var forceWeb = d3.layout.force()
+			.nodes(vertices)
+			.links(edges)
+			.size([width, height])
+			.on("tick", function (){
+				domVertices().attr("cx", function (vertex) { return vertex.x; })
+				   		     .attr("cy", function (vertex) { return vertex.y; });
+			   	
+			   	domEdges().attr("x1", function (edge) { return edge.source.x; })
+			   	          .attr("y1", function (edge) { return edge.source.y; })
+			   	          .attr("x2", function (edge) { return edge.target.x; })
+			   	          .attr("y2", function (edge) { return edge.target.y; });
+			});
+
+		return {
+			update: function (options) {
+				var edgeIsIncluded = function (edge) {
+					return options.nodeFilter(edge.source) && options.nodeFilter(edge.target);
+				};
+
+				edgeOn = domEdges().data(edges.filter(edgeIsIncluded), UIDkey);
+				
+				edgeOn.enter()
+					.append("line")
+					.attr("class", "link");
+
+				edgeOn.exit().remove();
+
+				edgeOn.style("stroke-width", options.strokeWidth);
 
 
 
-			vertexOn = domVertices().data(vertices.filter(options.nodeFilter), UIDkey);
-			
-			vertexOn.enter()
-			    .append("circle")
-			    .attr("class", "node")
-			    .attr("r", 9);
-			    
-			vertexOn.style("fill", options.nodeColor);
-			
-			vertexOn.exit().remove();
+				vertexOn = domVertices().data(vertices.filter(options.nodeFilter), UIDkey);
+				
+				vertexOn.enter()
+				    .append("circle")
+				    .attr("class", "node")
+				    .attr("r", 9);
+				    
+				vertexOn.style("fill", options.nodeColor);
+				
+				vertexOn.exit().remove();
 
 
 
-			forceWeb
-				.charge(function (node) {
-					return options.nodeFilter(node) ? options.charge(node) : 0;;
-				})
-				.linkStrength(function (link) {
-					return edgeIsIncluded(link) ? options.linkStrength(link) : 0;
-				})
-				.linkDistance(options.linkDistance)
-				.start();
+				forceWeb
+					.charge(funcOrZeroIfNot(options.charge, options.nodeFilter))
+					.linkStrength(funcOrZeroIfNot(options.linkStrength, options.nodeFilter))
+					.linkDistance(options.linkDistance)
+					.start();
+			}
 		}
 	}
-}
+})();
